114e1b53c9a45c162e10eb273684f1eb78db251f
Beatrice Johansson <annbeatrice.johansson@gmail.com>
2018-01-07 21:00:57 +0100

    third commit

--- javascript.js
+++ javascript.js
@@ -3,3 +3,16 @@ $("#burger").click(function(){
     $("#burger").toggleClass("change");
     $("#navigation").toggleClass("active");
 })
+
+$(".image_link").hover(
+    // Första function - mus in. "this" är vad som blir hovrat på! Så att bara den man hovrar på får styling och inte alla med samma class.
+    function(){
+        console.log("Der blir hovrat!")
+        $( this ).addClass( "active" );
+    },
+    function(){
+        console.log("Der blir udhovrat!")
+        $( this ).removeClass( "active" );
+    },
+
+        );
